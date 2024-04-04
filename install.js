91885d08f18bd85231ff10a36cce9446ed2df38f
Maamoun Okla <mamoun95bo@icloud.com>
2024-04-04 04:37:06 +0200

    Deploy_ready

--- install.js
+++ install.js
@@ -2,6 +2,26 @@
  * Databas install 
  * Av Maamoun Okla
  */
-const sqlite3 = require(sqlite3).verbos();
+const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.database("./db/cv.db");
+const db = new sqlite3.Database("./db/cv.db");
+
+
+//Skapa tabell för kurser 
+//( id | coursecode | coursename | syllabus | progression | posted)
+
+db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS courses;");
+    db.run(`
+    CREATE TABLE courses(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        coursecode INTEGER NOT NULL,
+        coursename TEXT NOT NULL,
+        syllabus TEXT NOT NULL,
+        progression TEXT NOT NULL,
+        posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        );    
+    `);
+})
+
+db.close();
--- server.js
+++ server.js
@@ -1,4 +1,81 @@
 /**
- * Databas server 
+ * CV server
  * Av Maamoun Okla
  */
+const express = require("express");
+const bodyParser = require("body-parser"); //Läsa form-data
+const sqlite3 = require("sqlite3").verbose();
+
+//Ansluta till databasen
+const db = new sqlite3.Database("./db/cv.db");
+
+//Inställningar
+const app = express();
+const port = 3001;
+app.set("view engine", "ejs"); //View engine: EJS
+app.use(express.static("public")); //Statiska filer i katalog "public"
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Routing
+
+app.get("/", (req, res) => {
+  //Läs ut befintliga kurser från courses
+  db.all("SELECT * FROM courses ORDER BY id DESC;", (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+
+    res.render("index", {
+      error: "",
+      rows: rows,
+    });
+  });
+});
+app.get("/addCourse", (req, res) => {
+  res.render("addCourse", {
+    error: "",
+  });
+});
+app.post("/addCourse", (req, res) => {
+  let courseName = req.body.courseName;
+  let courseCode = req.body.courseCode;
+  let progression = req.body.progression;
+  let syllabus = req.body.syllabus;
+  let error = "";
+  if (courseName != "" && courseCode != "" && syllabus != "") {
+    //korrekt - lagra i db
+
+    const stmt = db.prepare(
+      "INSERT INTO courses(courseName, courseCode, progression, syllabus)VALUES(?,?,?,?);"
+    );
+    stmt.run(courseName, courseCode, progression, syllabus);
+    stmt.finalize();
+  } else {
+    error = "Du måste fylla i alla fält!";
+  }
+
+  res.render("addCourse", {
+    error: error,
+  });
+});
+//Radera kurs
+app.get("/delete/:id", (req, res) => {
+  let id = req.params.id;
+
+  //Radera inlägg
+  db.run("DELETE FROM courses WHERE id=?;", id, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    //Redirekt till startsidan
+    res.redirect("/");
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+//Starta applikationen
+app.listen(port, () => {
+  console.log("Server started from port: " + port);
+});
